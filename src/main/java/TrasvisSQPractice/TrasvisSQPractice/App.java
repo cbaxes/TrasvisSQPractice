d63f25f6d69bfaa6429273757de36b90a47af0db
Cory Baxes <cbaxes@gmail.com>
2018-02-25 14:53:43 -0800

    Initial Eclipse project.

---
+++ src/main/java/TrasvisSQPractice/TrasvisSQPractice/App.java
@@ -0,0 +1,13 @@
+package TrasvisSQPractice.TrasvisSQPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
